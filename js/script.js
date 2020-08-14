1adee632a17602efe6e214587e78b4f0406cb14b
Benjamin Foster <bjfoster.wrk@gmail.com>
2020-08-14 15:23:01 +0100

    jQuery for auto opening modals via URL.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,16 @@ $(document).ready(function () {
     });
 });
 
+
+$(document).ready(function () {
+    $('.modal').each(function () {
+        const modalId = `#${$(this).attr('id')}`;
+        if (window.location.href.indexOf(modalId) !== -1) {
+            $(modalId).modal('show');
+        }
+    });
+});
+
 function reload1() { 
     document.getElementById('if1').src += ''; 
 } 
